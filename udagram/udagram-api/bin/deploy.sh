bbcab05dd7c68773fb7133b5e76c48a7e2652360
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2023-03-21 16:50:55 +0200

    as

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,7 +1,11 @@
-eb init udagram-api --platform node.js --region us-east-1
+# eb init udagram_fullstack --platform node.js --region us-east-1
+# eb create udagramfullstack-env --region us-east-1
+# eb use udagramfullstack-env
+eb deploy  
+eb health
+# eb open
 
-eb use abanob-udagram-api
+# if you want read the logs
+# eb logs
 
-eb setenv AWS_BUCKET=AWS_BUCKET POSTGRES_HOST=POSTGRES_HOST DB_PORT=DB_PORT POSTGRES_DB=POSTGRES_DB POSTGRES_USERNAME=POSTGRES_USERNAME POSTGRES_PASSWORD=POSTGRES_PASSWORD
-
-eb deploy abanob-udagram-api
+eb setenv AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" POSTGRES_USERNAME="$POSTGRES_USERNAME" POSTGRES_PASSWORD="$POSTGRES_PASSWORD" POSTGRES_DB="$POSTGRES_DB" PORT="$PORT" POSTGRES_HOST="$POSTGRES_HOST" AWS_REGION="$AWS_REGION" AWS_PROFILE="$AWS_PROFILE" AWS_BUCKET="$AWS_BUCKET" URL="$URL" JWT_SECRET="$JWT_SECRET" --profile default
